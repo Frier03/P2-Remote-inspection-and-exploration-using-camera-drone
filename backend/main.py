9882b5760079f99e2271addd726fa61aafd3954d
Sh1tters <72045321+Sh1tters@users.noreply.github.com>
2023-03-20 22:22:11 +0100

    Update OAuth2 on "/api/auth/logout" for authorization.
    Add-on Update from earlier OAuth2 implementation.
    
    SECRET_KEY is a fake key.

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+#https://fastapi.tiangolo.com/tutorial/security/oauth2-jwt/
 #uvicorn main:app --reload
 #pip install "python-jose[cryptography]"
 #pip install "passlib[bcrypt]"
@@ -12,7 +13,6 @@ from pydantic import BaseModel
 
 SECRET_KEY = "DuJwTmBr35qLU7HHqg2AMG+jkmx92JZk" #https://cloud.google.com/network-connectivity/docs/vpn/how-to/generating-pre-shared-key
 
-sessions = {}
 fake_users_db = {
     "admin": {
         "username": "admin",
@@ -38,8 +38,11 @@ class UserInDB(User):
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 #pwd_context.hash("password")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
+
+sessions = {}
 
 origins = [ # Which request the API will allow
     "http://localhost",
@@ -58,9 +61,28 @@ app.add_middleware(
 def handle(request: Request = None):
     return { "data": sessions }
 
-@app.get("/api/auth/logout")
-def handle(request: Request = None):
-    pass #is_user_authorized
+@app.post("/api/auth/logout")
+async def handle(request: Request = None):
+    request = await request.json()
+    access_token = request.get("access_token").split("access_token=")[1]
+    
+    # Decode access token
+    decoded_token = decode_access_token(access_token)
+    # Decode username from token
+    username = decoded_token.get('sub')
+    
+    # Validate token by username
+    if not is_user_authorized(username, access_token):
+        raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Not Authorized.",
+                headers={"WWW-Authenticate": "Bearer"}
+        )
+    
+    del sessions[username] #NOTE: Use the expiration date from the jwt token instead of using in-memory :)
+    
+    return { "message": "OK" }
+        
 
 @app.post("/api/auth/login", response_model=Token)
 async def handle(request: Request = None):
@@ -102,9 +124,21 @@ def authenticate_user(username: str, password: str):
         return False
     return user
 
+def is_user_authorized(username: str, access_token: str):
+    try:
+        # Validate access token
+        if sessions[username] == access_token:
+            return True
+        return False
+    except KeyError:
+        return False
+
 def generate_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=30)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm="HS256")
     return encoded_jwt
+
+def decode_access_token(encoded_jwt: str):
+    return jwt.decode(encoded_jwt, SECRET_KEY, algorithms=["HS256"])
