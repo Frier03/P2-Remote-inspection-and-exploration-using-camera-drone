ef2a19fe7d780dee2678933bd0c650f0547601ce
Sh1tters <72045321+Sh1tters@users.noreply.github.com>
2023-03-21 22:26:25 +0100

    Delete helperFunctions.js since it has no purpose anymore

--- app/src/helperFunctions.js
+++
@@ -1,7 +0,0 @@
-function trimAccessToken(token){
-    let fixedToken = token.split('access_token=')[1]
-    fixedToken = fixedToken.split(';')[0]
-    return fixedToken
-}
-
-export default trimAccessToken;
