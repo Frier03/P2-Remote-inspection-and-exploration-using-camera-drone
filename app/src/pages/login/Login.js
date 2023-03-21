6e255e87046dfc828f688edf7ea9a095056a4335
Sh1tters <72045321+Sh1tters@users.noreply.github.com>
2023-03-21 22:26:50 +0100

    Update fetch to work with API

--- app/src/pages/login/Login.js
+++ app/src/pages/login/Login.js
@@ -1,47 +1,63 @@
-import trimAccessToken from '../../helperFunctions'
+import React, { useState } from 'react';
+import Cookies from 'js-cookie';
 
-function handleLogin(){
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-    let token = document.cookie;
-    token = trimAccessToken(token);
+function LoginForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [token, setToken] = useState('');
 
-    // send a POST request to backend
+  // everything inside of the LoginForm() and outside of the handleSubmit will run whenever a user refreshes or uses the url. This means we can implement authorization here.
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
     fetch('http://127.0.0.1:8000/v1/auth/login', {
         method: 'POST',
         headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            username: username, 
-            password: password,
-            access_token: token
+            name: username, 
+            password: password
         })
     })
-    .then(res => Promise.all([res.status, res.json()]))
-    .then(([status, data]) => {
-        // Make proper logic here
-        console.log(status, data)
-        const access_token = data['access_token'];
-        document.cookie = 'access_token='+access_token+";";
+    .then(response => response.json())
+    .then(data => {
+        if (data.access_token) {
+            setToken(data.access_token);
+            Cookies.set('access_token', data.access_token)
+        } else {
+            alert('Invalid credentials');
+        }
     })
     .catch(error => {
-        // handle any errors here
-        console.error(error);
+        if (error.message === 'Failed to fetch') {
+            alert('API is offline')
+        } else {
+            alert('An error occured')
+        }
     })
+  }
+  if (token) {
+    // Redirect to the protected route
+    window.location = '/'
+    
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={event => setUsername(event.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={event => setPassword(event.target.value)} />
+      </label>
+      <button type="submit">Login</button>
+    </form>
+  );
 }
 
-const Login = () => {
-    return (
-    <>
-    <h1>Login</h1>
-    <label>Username </label>
-    <input type="text" id="username"/>
-    <label>Password </label>
-    <input type="text" id="password"/>
-    <button onClick={handleLogin}>Login here</button>
-    </>
-    );
-  }
-export default Login;
+export default LoginForm;
--- app/src/pages/logout/Logout.js
+++ app/src/pages/logout/Logout.js
@@ -1,36 +1,41 @@
-import trimAccessToken from '../../helperFunctions'
+import React, { useState } from 'react';
+import Cookies from 'js-cookie';
 
-function handleLogout(){
-    // retrieve the JWT token from the cookie
-    let token = document.cookie;
-    token = trimAccessToken(token)
- 
-    // send a POST request to backend
+function LogoutForm( {token} ) {
+
+  function handleLogout(event) {
+    event.preventDefault();
+
     fetch('http://127.0.0.1:8000/v1/auth/logout', {
         method: 'POST',
         headers: {
-            'Authorization': `Bearer ${token}`,
+            'Authorization': `Bearer ${Cookies.get('access_token')}`,
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         }
     })
-    .then(res => Promise.all([res.status, res.json()]))
-    .then(([status, data]) => {
-        // Make proper logic here
-        console.log(status, data)
+    .then(response => response.json())
+    .then(data => {
+        if(data.message === "OK"){
+            //Cookies.set('access_token', 'NULL') # Since backend adds token to a blacklist, we do not need this
+        }
     })
     .catch(error => {
-        // handle any errors here
-        console.error(error);
+        if (error.message === 'Failed to fetch') {
+            alert('API is offline')
+        } else {
+            alert('An error occured')
+        }
     })
-}
-
-const Logout = () => {
-    return (
-    <>
-    <h1>Logout</h1>
-    <button onClick={handleLogout}>Logout</button>
-    </>
-    );
   }
-export default Logout;
+
+  return (
+    <form onSubmit={handleLogout}>
+      <label>
+        Logout
+        <button type="submit">Logout</button>
+      </label>
+    </form>
+  );
+}
+export default LogoutForm;
