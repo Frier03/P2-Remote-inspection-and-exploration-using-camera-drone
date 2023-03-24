9832a8de171d60c412f2b4715b06cf80d82c8635
Sh1tters <72045321+Sh1tters@users.noreply.github.com>
2023-03-24 15:04:10 +0100

    Optimization with @Mikkel-Coder @Tobi7552

--- backend/main.py
+++ backend/main.py
@@ -7,17 +7,13 @@ from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from jose import JWTError, jwt #jwt & pyjwt
 from passlib.context import CryptContext
 from pydantic import BaseModel
-import time
 
-from starlette.exceptions import HTTPException as starletteHTTPException
 from starlette.responses import HTMLResponse as starletteHTMLResponse 
-
 from os import getenv
 
-SECRET_KEY = getenv('SECRET_KEY')
-
-# // FILES
 from frontend_origins import add_origins
+
+SECRET_KEY = str(getenv('SECRET_KEY'))
 
 fake_users_db = { #TODO: Use MongoDB
     "admin": {
@@ -26,10 +22,11 @@ fake_users_db = { #TODO: Use MongoDB
     }
 }
 
-blacklisted_tokens = {} #NOTE: Better to store in-memory
+blacklisted_tokens = {} #NOTE: Use database
 
 routes_with_middleware = [
-    "/v1/auth/protected"
+    "/v1/auth/protected",
+    "/v1/auth/logout"
 ]
 
 class Token(BaseModel):
@@ -48,8 +45,9 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 @app.middleware("http")
 async def authorization(request: Request, call_next):
-    if request.url.path not in routes_with_middleware:
-        return await call_next(request) # Routes with no authentication
+    # Routes with no authentication
+    if request.url.path not in routes_with_middleware: # Routes with no authentication
+        return await call_next(request)
     
     # Get access token from headers
     access_token = request.headers.get('authorization')
@@ -75,18 +73,19 @@ async def authorization(request: Request, call_next):
     # Await endpoint
     response = await call_next(request)
 
-    # Decode access token
-    payload = decode_access_token(access_token)
-    username = payload.get('sub')
+    if request.url.path != "/v1/auth/logout":
+        # Decode access token
+        payload = decode_access_token(access_token)
+        username = payload.get('sub')
 
-    # Generate new HS256 access token
-    new_access_token = generate_access_token(data={"sub": username})
+        # Generate new HS256 access token
+        new_access_token = generate_access_token(data={"sub": username})
 
-    # Edit authorization header with new access token
-    response.headers['WWW-Authenticate'] = f"Bearer {new_access_token}"
+        # Edit authorization header with new access token
+        response.headers['WWW-Authenticate'] = f"Bearer {new_access_token}"
 
-    # Create expiration date of 2 hours
-    expire = datetime.utcnow() + timedelta(minutes=1440)
+    # Create expiration date of 24 hours
+    expire = datetime.utcnow() + timedelta(minutes=24*60)
     expire = expire.timestamp()
     
     # Store token and expiration in blacklisted_tokens
@@ -98,56 +97,45 @@ async def authorization(request: Request, call_next):
 def handle():
     return { "blacklisted tokens": blacklisted_tokens }
 
-@app.get("/v1/auth/protected") #NOTE Check if this works
+@app.get("/v1/auth/protected")
 def handle():
     return { "message": "Authorized" }
 
 @app.post("/v1/auth/logout")
-async def handle(credentials: HTTPAuthorizationCredentials = Depends(bearer)):
-    # Validate token
-    if not is_user_authorized(credentials.credentials):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-    
-    # Create expiration date of 2 hours
-    expire = datetime.utcnow() + timedelta(minutes=1440)
-    expire = expire.timestamp()
-    
-    # Store token and expiration in blacklisted_tokens
-    blacklisted_tokens[expire] = credentials.credentials
-    
-    return { "message": "OK" }
-        
+async def handle():
+    return { "message": "Logout" }
+
 
 @app.post("/v1/auth/login")
 async def handle(user: User):        
-        if not authenticate_user(user.name, user.password):
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Incorrect username or password",
-                headers={"WWW-Authenticate": "Bearer"}
-            )
-            
-        # Encode user.name
-        encoded_name = jwt.encode({'sub': user.name}, SECRET_KEY, algorithm="HS256")
+    if not authenticate_user(user.name, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
         
-        # Remove everything that not the encoded user.name
-        encoded_name = encoded_name.split('.')[0]
+    # Encode user.name
+    encoded_name = jwt.encode({'sub': user.name}, SECRET_KEY, algorithm="HS256")
+    
+    # Remove everything that not the encoded user.name
+    encoded_name = encoded_name.split('.')[0]
+    
+    # Map the encoded user names to their corresponding keys in blacklisted tokens
+    header_to_token = {key.split(".")[0]: key for key in blacklisted_tokens}
+    
+    # Is user.name (encoded in HS256) present in the map?
+    if encoded_name in header_to_token:
+
+        # Delete blacklisted token
+        del blacklisted_tokens[header_to_token[encoded_name]]
         
-        # Map the encoded user names to their corresponding keys in blacklisted tokens
-        header_to_token = {key.split(".")[0]: key for key in blacklisted_tokens}
-        
-        # Is user.name (encoded in HS256) present in the map?
-        if encoded_name in header_to_token:
-  
-            # Delete blacklisted token
-            del blacklisted_tokens[header_to_token[encoded_name]]
-            
-        # Generate new HS256 access token
-        token = generate_access_token(data={"sub": user.name})
-        return {"access_token": token, "token_type": "bearer"}
+    # Generate new HS256 access token
+    token = generate_access_token(data={"sub": user.name})
+    return {"access_token": token, "token_type": "bearer"}
 
 
-def verify_password(plain_password: str, hashed_password: str) -> bool:
+def verify_password(plain_password: str, hashed_password: str) -> bool: #TODO Make this more secure
     # Placeholder for validating the password
     if len(plain_password) < 3:
         return False
