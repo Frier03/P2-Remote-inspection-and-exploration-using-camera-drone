356553d726298a410b7ff6721ccb54ef5a07dbb1
simonsvale <s.risbjerg@stofanet.dk>
2023-03-24 10:39:49 +0100

    Made AP mode work with feed.

--- relay/set_ap_mode.py
+++ relay/set_ap_mode.py
@@ -3,7 +3,7 @@ import socket
 host_ip = ''
 host_port = 11111
 
-tello_ip = '192.168.137.112'
+tello_ip = '192.168.10.1'
 tello_port = 8889
 
 # Create UDP socket
@@ -11,13 +11,6 @@ video_udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 video_udp_socket.bind((host_ip, host_port))
 
-# Initialize sdk 
+# Initialize sdk and reset password
 video_udp_socket.sendto(bytes("command", 'utf-8'), (tello_ip, tello_port))
-video_udp_socket.sendto(bytes("streamon", 'utf-8'), (tello_ip, tello_port))
-
-print('Done')
-
-while True:
-    response, ip = video_udp_socket.recvfrom(2048)
-
-    print(response)
+video_udp_socket.sendto(bytes("ap LAPTOP_TEST HardwareSuperTest", 'utf-8'), (tello_ip, tello_port))
