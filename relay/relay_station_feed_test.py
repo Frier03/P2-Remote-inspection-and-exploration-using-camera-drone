717cb49dde2f50cc881dcd6b5d0ae5c3f08ac1e8
simonsvale <s.risbjerg@stofanet.dk>
2023-03-24 10:41:30 +0100

    Solved problem with sending feed in access point mode.

--- relay/relay_station_feed_test.py
+++ relay/relay_station_feed_test.py
@@ -6,7 +6,7 @@ host_port = 11111
 tello_ip = '192.168.137.112'
 tello_port = 8889
 
-# Create UDP socket
+# Create UDP socket.
 video_udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 video_udp_socket.bind((host_ip, host_port))
