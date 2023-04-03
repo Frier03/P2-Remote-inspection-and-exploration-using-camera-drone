b75b10b50cde106960dda2f3ed33d9899de33398
simonsvale <s.risbjerg@stofanet.dk>
2023-04-03 17:04:56 +0200

    Added a fix for computers that would not verify the certification.

--- backend/mongodb_handler.py
+++ backend/mongodb_handler.py
@@ -1,10 +1,15 @@
-import pymongo
+import pymongo, certifi
 MONGODB_USERNAME="admin"
 MONGODB_PASSWORD="kmEuqHYeiWydyKpc"
+
+# Needed for some computers D: - Simon 
+ca = certifi.where()
 
 
 connection_url = f"mongodb+srv://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@cluster.eoaspgc.mongodb.net/?retryWrites=true&w=majority"
 print(connection_url)
-client = pymongo.MongoClient(connection_url)
+
+# tlsCAFile=ca is a part of the fix
+client = pymongo.MongoClient(connection_url, tlsCAFile=ca)
 database = client['Backend']
 print(database.list_collection_names())
