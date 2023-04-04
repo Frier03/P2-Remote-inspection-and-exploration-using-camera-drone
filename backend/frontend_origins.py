9486f1fce7f6edad7897f6ac6445b8d996af6560
Sh1tters <justinbestworldwide@gmail.com>
2023-04-04 19:06:24 +0200

    All Origins now have request access to backend

--- backend/frontend_origins.py
+++ backend/frontend_origins.py
@@ -10,7 +10,7 @@ def add_origins(app: object):
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=origins,
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"]
