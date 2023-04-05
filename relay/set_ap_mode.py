3f5d5ec2076956e65ad8664040c83a8b132f9f25
simonsvale <s.risbjerg@stofanet.dk>
2023-04-05 09:22:03 +0200

    Added a function to change the video port of the drone.

--- relay/set_ap_mode.py
+++ relay/set_ap_mode.py
@@ -1,4 +1,4 @@
-import socket
+import socket, time
 
 host_ip = ''
 host_port = 11111
@@ -7,10 +7,24 @@ tello_ip = '192.168.10.1'
 tello_port = 8889
 
 # Create UDP socket.
-video_udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-video_udp_socket.bind((host_ip, host_port))
+udp_socket.bind((host_ip, host_port))
 
 # Initialize sdk and reset password
-video_udp_socket.sendto(bytes("command", 'utf-8'), (tello_ip, tello_port))
-video_udp_socket.sendto(bytes("ap LAPTOP_TEST HardwareSuperTest", 'utf-8'), (tello_ip, tello_port))
+udp_socket.sendto(bytes("command", 'utf-8'), (tello_ip, tello_port))
+status1 = udp_socket.recvfrom(128)
+print(status1)
+
+# the the first integer is the port for status the second is the video port.
+udp_socket.sendto(bytes("port 8889 2003", 'utf-8'), (tello_ip, tello_port))
+status2 = udp_socket.recvfrom(128)
+print(status2)
+
+"""
+time.sleep(5)
+udp_socket.sendto(bytes("ap LAPTOP_TEST HardwareSuperTest", 'utf-8'), (tello_ip, tello_port))
+print("Set to access point mode")
+status3 = udp_socket.recvfrom(128)
+print(status3)
+"""
