ef12186273274925a75a163ad43a7f04e9476dad
simonsvale <s.risbjerg@stofanet.dk>
2023-04-05 10:38:56 +0200

    Testing multiple video feed over same access point.

--- relay/set_ap_mode.py
+++ relay/set_ap_mode.py
@@ -16,15 +16,7 @@ udp_socket.sendto(bytes("command", 'utf-8'), (tello_ip, tello_port))
 status1 = udp_socket.recvfrom(128)
 print(status1)
 
-# the the first integer is the port for status the second is the video port
-udp_socket.sendto(bytes("port 8889 2003", 'utf-8'), (tello_ip, tello_port))
-status2 = udp_socket.recvfrom(128)
-print(status2)
-
-"""
-time.sleep(5)
 udp_socket.sendto(bytes("ap LAPTOP_TEST HardwareSuperTest", 'utf-8'), (tello_ip, tello_port))
 print("Set to access point mode")
-status3 = udp_socket.recvfrom(128)
-print(status3)
-"""
+status2 = udp_socket.recvfrom(128)
+print(status2)
