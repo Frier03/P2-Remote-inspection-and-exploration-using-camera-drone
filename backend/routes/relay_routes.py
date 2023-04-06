1af7b5f4751ffc01358c14fc9c6cb7db0e1d8dbe
Sh1tters <justinbestworldwide@gmail.com>
2023-04-06 08:29:08 +0200

    Removed unused functions

--- backend/routes/relay_routes.py
+++ backend/routes/relay_routes.py
@@ -1,12 +1,10 @@
 from fastapi import HTTPException, status, APIRouter, Depends
-from passlib.context import CryptContext
 
-from token_helper_functions import generate_access_token
+from helper_functions import generate_access_token
 from mongodb_handler import get_mongo
 from models import DroneModel, RelayHandshakeModel
 from relaybox import Relay
 
-pwd_context = CryptContext(schemes=["bcrypt_sha256"])
 relay_router = APIRouter()
 active_relays = {}
 
@@ -89,7 +87,7 @@ def handle(drone: DroneModel):
 
 @relay_router.post("/handshake")
 def handle(relay: RelayHandshakeModel, mongo: object = Depends(get_mongo)):
-    if not authenticate_relay(relay, mongo):
+    if not mongo.authenticate(relay, mongo):
          raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED)
 
@@ -102,18 +100,3 @@ def handle(relay: RelayHandshakeModel, mongo: object = Depends(get_mongo)):
     # Generate new HS256 access token
     token = generate_access_token(data={'sub': relay.name}, minutes=24*60)
     return {"access_token": f"Bearer {token}"}
-
-
-def authenticate_relay(relay: RelayHandshakeModel, mongo: object) -> bool:
-    relay_exist = mongo.name_exist({ 'name': relay.name }, mongo.relays_collection)
-    if not relay_exist:
-        return False
-    if not verify_password(relay.password, relay_exist.get('hashed_password')):
-        return False
-    return True
-
-def verify_password(plain_password: str, hashed_password: str) -> bool: #TODO Make this more secure
-    # Placeholder for validating the password
-    if len(plain_password) < 3:
-        return False
-    return pwd_context.verify(plain_password, hashed_password) # True/False depends if both hashes passwords matches
