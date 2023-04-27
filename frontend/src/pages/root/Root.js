58d7d7c45bd0a2f766aec2396ceb7f47a5d8cf7a
Sh1tters <justinbestworldwide@gmail.com>
2023-04-27 08:33:50 +0200

    RelaySidebar now fetches data from backend every 1000ms

--- frontend/src/pages/root/Root.js
+++ frontend/src/pages/root/Root.js
@@ -1,20 +1,13 @@
 import withAuthorization from "../../authzVerify";
-import WorkerExample from "./WorkerExample";
+import RelaysSidebar from "./components/RelaysSidebar";
 import "./Root.css";
 
-
 function RootForm({ authorizationStatus }) {
-  // create a new worker and pass the worker script file as an argument
-  var worker = new Worker("worker.js");
-  // Start worker
-  // worker gets data from backend every 10 sec
-  // convert data into html
   if (authorizationStatus === "Authorized") {
     return (
       <>
-            <div>
-      <h1>Hello World</h1>
-      <WorkerExample />
+        <div>
+          <RelaysSidebar/>
         </div>
       </>
     );
